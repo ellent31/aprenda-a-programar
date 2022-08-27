90fd4e2fb2067c3228ea3f45cb4128f1d481d6b5
Ellen Tairine <contatosocial22@gmail.com>
2022-08-26 21:54:54 -0300

    meu-quarto-codigo

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,3 +3,9 @@ let email = "contatosocial22@gmail.com"
 let profissao = "Dev Web Junior"
 let idade = "31"
 let lugar = "Punta Cana"
+
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(lugar)
