93257bb31193f75b9b73df649802241a278c1ae0
Ellen Tairine <contatosocial22@gmail.com>
2022-08-27 17:10:37 -0300

    meu-quarto-codigo

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,7 +1,7 @@
 let nome = "Ellen Tairine"
 let email = "contatosocial22@gmail.com"
 let profissao = "Dev Web Junior"
-let idade = "31"
+let idade = 31
 let lugar = "Punta Cana"
 
 console.log(nome)
@@ -9,3 +9,6 @@ console.log(email)
 console.log(profissao)
 console.log(idade)
 console.log(lugar)
+
+console.log(`Olá, ${nome}! Somos aqui da agência de turismo Viagem dos sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${lugar}. Confirma pra gente alguns dados? A sua idade é ${idade} você é ${profissao} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D `)
+ 
